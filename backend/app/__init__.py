3f93e86dc8040728a70142b8da7741a692295659
komfysach <sach@getkomfy.net>
2021-05-21 13:17:44 +0200

    completed backend blockchain

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,23 @@
-from backend.blockchain.block import Block
+import json
 import os
 import requests
 import random
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
+from backend.wallet.transactions import Transaction
+from backend.blockchain.block import Block
 from backend.blockchain.blockchain import Blockchain
+from backend.wallet.wallet import Wallet
+from backend.wallet.transactions import Transaction
+from backend.wallet.transaction_pool import TransactionPool
 from backend.pubsub import PubSub
 
 app = Flask(__name__)
 blockchain = Blockchain()
-pubsub = PubSub(blockchain)
+wallet = Wallet(blockchain)
+transaction_pool = TransactionPool()
+pubsub = PubSub(blockchain, transaction_pool)
 
 @app.route('/')
 def route_default():
@@ -22,14 +29,40 @@ def route_blockchain():
 
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
-    transaction_data = 'stubbed_transaction_data'
-    
+    transaction_data = transaction_pool.transaction_data()
+    transaction_data.append(Transaction.reward_transaction(wallet).to_json())
     blockchain.add_block(transaction_data)
-    
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
+    transaction_pool.clear_blockchain_transactions(blockchain)
     
     return jsonify(block.to_json())
+
+@app.route('/wallet/transact', methods=['POST'])
+def route_wallet_transact():
+    transaction_data = request.get_json()
+    transaction = transaction_pool.existing_transaction(wallet.address)
+    
+    if transaction:
+        transaction.update(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+    else:
+        transaction = Transaction(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+    
+    pubsub.broadcast_transaction(transaction)
+    
+    return jsonify(transaction.to_json())
+
+@app.route('/wallet/info')
+def route_wallet_info():
+    return jsonify({'address': wallet.address, 'balance': wallet.balance})
    
 ROOT_PORT = 5000
 PORT = ROOT_PORT
--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,6 +1,6 @@
-from _pytest.python_api import raises
-from flask.json import jsonify
-from backend.tests.blockchain.test_block import block, last_block
+from backend.wallet.wallet import Wallet
+from backend.config import MINING_REWARD_INPUT
+from backend.wallet.transactions import Transaction
 from backend.blockchain.block import Block
 
 class Blockchain:
@@ -68,8 +68,51 @@ class Blockchain:
             block = chain[i]
             last_block = chain[i-1]
             Block.is_valid_block(last_block, block)
+            
+        Blockchain.is_valid_transaction_chain(chain)
+            
+    @staticmethod
+    def is_valid_transaction_chain(chain):
+        """
+        Enforce the rules of a chain composed of blocks of transactions.
+            - Each transaction must only appear once in the chain.
+            - There can only be one mining reward per block.
+            - Each transaction must be valid.
+        """
+        transaction_ids = set()
         
-
+        for i in range(len(chain)):
+            block = chain[i]
+            has_mining_reward = False
+            
+            for transaction_json in block.data:
+                transaction = Transaction.from_json(transaction_json)
+                
+                if transaction.id in transaction_ids:
+                    raise Exception(f'Transaction {transaction.id} is not unique')
+                
+                transaction_ids.add(transaction.id)
+                
+                if transaction.input == MINING_REWARD_INPUT:
+                    if has_mining_reward:
+                        raise Exception(
+                            'There can only be one mining reward per block.' \
+                             f'Check block with hash: {block.hash}'
+                        )
+                    has_mining_reward = True
+                else:
+                    historic_blockchain = Blockchain()
+                    historic_blockchain.chain = chain[0:i]
+                    historic_balance = Wallet.calculate_balance(
+                    historic_blockchain,
+                    transaction.input['address']
+                    )
+                
+                    if historic_balance != transaction.input['amount']:
+                        raise Exception(f'Transaction {transaction.id} has an invalid input amount')
+                
+                Transaction.is_valid_transaction(transaction)
+        
 def main():        
         blockchain = Blockchain()
         blockchain.add_block('one')
